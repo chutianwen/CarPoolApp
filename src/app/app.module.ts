81bb569abe54fcd8cabfe10ed275fe74a1167dbe
chutianwen <tianwen.chu@fedcentric.com>
2018-01-28 09:08:16 -0500

    eding router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
 
 import {AgmCoreModule} from '@agm/core';
 import {GoogleMapComponent} from './google-map/google-map.component';
@@ -14,6 +16,9 @@ import {AngularFireAuthModule} from "angularfire2/auth";
 import {Ng4GeoautocompleteModule} from "ng4-geoautocomplete";
 import {OwlDateTimeModule, OwlNativeDateTimeModule} from "ng-pick-datetime";
 import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-browser/animations";
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ClarityModule } from "@clr/angular";
+import { NavComponent } from './nav/nav.component';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBqvRd9oGomx7YejW1NwCMsAHpGqZmRaiQ",
@@ -29,6 +34,7 @@ export const firebaseConfig = {
     BrowserModule,
     CommonModule,
     FormsModule,
+    ClarityModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBcm5AnNDQq_IOMjSJ3bnw-96Y3TSYFKDE',
       libraries: ["places"]
@@ -39,6 +45,12 @@ export const firebaseConfig = {
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     Ng4GeoautocompleteModule.forRoot(),
+    RouterModule.forRoot([
+      { path:'',component: GoogleMapComponent },
+      { path:'add',component: UserAddressManageComponent },
+      { path: '**',component: NotFoundComponent }
+    ]),
+    Ng4GeoautocompleteModule.forRoot(),
     ReactiveFormsModule,
     OwlDateTimeModule,
     OwlNativeDateTimeModule,
@@ -46,10 +58,10 @@ export const firebaseConfig = {
     NoopAnimationsModule
   ],
   providers: [],
-  declarations: [AppComponent, GoogleMapComponent, UserAddressManageComponent],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, GoogleMapComponent, UserAddressManageComponent, NotFoundComponent, NavComponent ],
+  bootstrap: [ AppComponent ]
 })
 
 
-export class AppModule {
-}
+
+export class AppModule {}
